eaf7c68ca089f6a57ae78edef7cfd08189feea23
Igor Zakutynsky <zakutynsky@yahoo.com>
2017-10-15 18:30:54 +0300

    next step

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,95 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg',
+            'title' => 'Harry Potter',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'price' => '10'
+        ]);
+
+        $product->save();
+    }
+}
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,19 +5,23 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://vignette.wikia.nocookie.net/harrypotter/images/2/2c/Goblet_of_Fire_Film_Poster.jpg" alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product title</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product->imagePath  }}" alt="{{$product->title}}" class="img-responsive">
+                        <div class="caption">
+                            <h3>{{$product->title}}</h3>
+                            <p>{{$product->description}}</p>
+                            <div class="clearfix">
+                                <div class="pull-left price">${{$product->price}}</div>
+                                <a href="#" class="btn btn-success pull-right" role="button">Button</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-    </div>
+    @endforeach
 @endsection
